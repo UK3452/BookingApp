bd268ac1f25b11ec5d0e8b6b121746f0b2bccb74
Utkarsh Kumar <kumar6.utkarsh@gmail.com>
2023-07-11 15:58:38 +0530

    project imported

---
+++ client/src/context/SearchContext.js
@@ -0,0 +1,42 @@
+import { createContext, useReducer } from "react";
+
+const INITITAL_STATE = {
+  city: undefined,
+  dates: [],
+  options: {
+    adult: undefined,
+    children: undefined,
+    room: undefined,
+  },
+};
+
+export const SearchContext = createContext(INITITAL_STATE);
+
+const SearchReducer = (state, action) => {
+  switch (action.type) {
+    case "NEW_SEARCH":
+      return action.payload;
+    case "RESET_SEARCH":
+      return INITITAL_STATE;
+    default:
+      return state;
+  }
+};
+
+export const SearchContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(SearchReducer, INITITAL_STATE);
+
+  return (
+    <SearchContext.Provider
+      value={{
+        city: state.city,
+        dates: state.dates,
+        options: state.options,
+        dispatch,
+      }}
+    >
+      {" "}
+      {children}{" "}
+    </SearchContext.Provider>
+  );
+};
